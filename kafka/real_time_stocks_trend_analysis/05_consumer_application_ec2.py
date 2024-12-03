79fb2a6a9e13738cc79408fe481a200fb541af8a
swaroop <swaroopiit@gmail.com>
2024-12-03 16:50:16 +0530

    Added some more content

--- kafka/real_time_stocks_trend_analysis/05_consumer_application_ec2.py
+++ kafka/real_time_stocks_trend_analysis/05_consumer_application_ec2.py
@@ -1,14 +1,27 @@
+from dotenv import load_dotenv
+import os
 from kafka import KafkaConsumer
-import json
 import time
 import boto3
 
+
 consumer = KafkaConsumer("cricket", bootstrap_servers="3.91.5.89:9092")
 
-s3_client = boto3.client("s3")
+# Get aws credentials from .env file
+load_dotenv()
+
+# boto3 session
+session = boto3.Session(
+    aws_access_key_id=os.getenv("aws_access_key_id"),
+    aws_secret_access_key=os.getenv("aws_secret_access_key"),
+)
+
+s3_client = session.client("s3")
 for data in consumer:
     # Convert data to JSON
     data = data.value
+
+    s3 = boto3.resource("s3")
 
     # Store file in S3
     file_name = "user_reg_data_" + time.strftime("%Y%m%d-%H%M%S") + ".json"
